152aeff183a5eb66f974c7cb5c66ead7d85502c9
Elias <elias.db3@gmail.com>
2023-12-04 13:06:35 +0100

    first commit

---
+++ floating-island/evefrfe/src/App.jsx
@@ -0,0 +1,14 @@
+import { Canvas } from '@react-three/fiber';
+
+import './App.css'
+import { SceneContainer } from './components/SceneContainer';
+
+function App() {
+  return (
+    <Canvas>
+      <SceneContainer />
+    </Canvas>
+  );
+}
+
+export default App
